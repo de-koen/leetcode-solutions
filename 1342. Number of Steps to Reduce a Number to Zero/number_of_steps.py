f7319dd970dfd6ce709365994bfa2204a83ee512
Denis Kostenok <lektorat228@gmail.com>
2022-10-23 16:37:56 +0300

    description refactored

--- 1342. Number of Steps to Reduce a Number to Zero/number_of_steps.py
+++ 1342. Number of Steps to Reduce a Number to Zero/number_of_steps.py
@@ -1,6 +1,5 @@
 #Simple solution
-"""
-def numberOfSteps(self, num):
+def number_of_steps_simple(self, num):
     steps = 0
     while (num > 0):
         if (num % 2) == 0:
@@ -9,10 +8,8 @@ def numberOfSteps(self, num):
             num -= 1
         steps += 1
     return steps
-"""
 
 #Wise solution
-#Explanation:
 '''
 Last digit in binary representation of a number tells us if it's even or odd. If the last digit is 0 then it's even, 
 if it's 1 then it's odd. When the number is odd we subtract 1 which makes last the last digit 0. When the number is 
@@ -21,10 +18,8 @@ even we divide it by 2 which equivelent to shifting bits to the right by 1. To m
 while for 0 number of steps is 1 (divide). So the anwser will be the (length of the binary representation - 1) + 
 the number of 1 in it. Exception: zero as input.
 '''
-"""
-def numberOfSteps(self, num):
+def number_of_steps_wise(self, num):
         if (num != 0): 
             return(num.bit_length() - 1 + bin(num).count("1"))
         else:
             return 0
-"""
