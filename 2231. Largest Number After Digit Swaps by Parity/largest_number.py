a37bad0c5a66d53100227f95c710e515a6b3424a
Denis Kostenok <lektorat228@gmail.com>
2022-10-23 17:01:45 +0300

    code as a function update

--- 2231. Largest Number After Digit Swaps by Parity/largest_number.py
+++ 2231. Largest Number After Digit Swaps by Parity/largest_number.py
@@ -3,27 +3,26 @@ Creates list of digits from input number,
 sorts odd and even digits and puts them into original positions 
 by their parity and in descending order 
 """
-num = int(input())
+def largest_number_after_permutation(num):
+    digits = list(map(int, str(num)))
+    odd = []
+    even = []
 
-digits = list(map(int, str(num)))
-odd = []
-even = []
-
-for digit in digits:
+    for digit in digits:
         if(digit % 2 == 0):
             even.append(digit)
         else:
             odd.append(digit)
 
-odd.sort()
-even.sort()
-result = ""
+    odd.sort()
+    even.sort()
+    result = ""
 
-for digit in digits:
+    for digit in digits:
         if(digit % 2 == 0):
             result += str(even.pop())
         else:
             result += str(odd.pop())
             
-print(result)
+    return result
             
