af218a2fe927503b05436d3f646725427bed0c5c
Denis Kostenok <lektorat228@gmail.com>
2022-10-26 22:53:26 +0300

    looked up solution

--- 821. Shortest Distance to a Character/shortest_to_char.py
+++ 821. Shortest Distance to a Character/shortest_to_char.py
@@ -1,14 +1,15 @@
-string = str(input())
-letter = str(input())
+from itertools import chain
 
-indices = [i for i, char in enumerate(string) if letter == char]
+s = str(input())
+c = str(input())
 
-result = []
-
-for i in range(len(string)):
-    if (len(indices) > 1):
-        if abs(i - indices[0]) >= abs(i - indices[1]):
-            indices.pop(0)
-    result.append(abs(i - indices[0]))
+#two passes concatenated in one
+n = len(s)
+pos = -n
+res = [n] * n
+for i in chain(range(n), range(n)[::-1]):
+    if s[i] == c:
+        pos = i
+    res[i] = min(res[i], abs(i - pos))
         
-print(result)
+print(res)
